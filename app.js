0e3bb34f22949fd5a88a11839674afb41f67d6d2
JustKevinn <kevinrodriguez7411@gmail.com>
2022-06-21 22:00:52 -0300

    agrego comentarios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let express = require('express')
-let path = require('path')
-let app = express()
+let path = require('path')           //require de modulos
+let app = express()  
 let port = 3000
 
 app.use(express.static(path.join(__dirname,'public')))
@@ -8,4 +8,5 @@ app.use(express.static(path.join(__dirname,'public')))
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
 
 app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}\n${__dirname}`))
+//ruta del servidor corriendo
 
